c0a99709b3f628c2c3f423ba06c5076ebbf5d529
Hendrizzzz <jimhendrixbageo@gmail.com>
2024-11-25 23:33:20 +0800

    Added the models

--- app/src/main/java/com/angelo_bageo_ferrer_matulay_sambot_tinggaan/noted/model/Note.kt
+++ app/src/main/java/com/angelo_bageo_ferrer_matulay_sambot_tinggaan/noted/model/Note.kt
@@ -1,4 +1,55 @@
 package com.angelo_bageo_ferrer_matulay_sambot_tinggaan.noted.model
 
+import java.time.LocalDate
+
 class Note {
+    private var title: String
+    private var content: String
+    private var date: LocalDate
+    private val author : User
+    private var isDeleted : Boolean
+
+    constructor(title : String, content : String, date : LocalDate, author : User) {
+        this.title = title
+        this.content = content
+        this.date = date
+        this.author = author
+        this.isDeleted = false
+    }
+
+    fun getTitle() : String {
+        return this.title
+    }
+
+    fun getContent() : String {
+        return this.content
+    }
+
+    fun getDate() : LocalDate {
+        return this.date
+    }
+
+    fun getAuthor() : User {
+        return this.author
+    }
+
+    fun isDeleted() : Boolean {
+        return this.isDeleted
+    }
+
+    fun setTitle(title : String) {
+        this.title = title
+    }
+
+    fun setContent(content : String) {
+        this.content = content
+    }
+
+    fun setDate(date : LocalDate) {
+        this.date = date
+    }
+
+    fun setDeleted(isDeleted : Boolean) {
+        this.isDeleted = isDeleted
+    }
 }
--- app/src/main/java/com/angelo_bageo_ferrer_matulay_sambot_tinggaan/noted/model/User.kt
+++ app/src/main/java/com/angelo_bageo_ferrer_matulay_sambot_tinggaan/noted/model/User.kt
@@ -1,4 +1,57 @@
 package com.angelo_bageo_ferrer_matulay_sambot_tinggaan.noted.model
 
+import java.time.LocalDate
+
 class User {
+    private var email: String
+    private var firstName: String
+    private var lastName: String
+    private var birthDate: LocalDate
+    private var password: String
+
+    constructor(email : String, firstName : String, lastName : String, birthDate : LocalDate, password : String){
+        this.email = email
+        this.firstName = firstName
+        this.lastName = lastName
+        this.birthDate = birthDate
+        this.password = password
+    }
+
+    fun getEmail() : String {
+        return this.email
+    }
+
+    fun getFirstName() : String {
+        return this.firstName
+    }
+
+    fun getLastName() : String {
+        return this.lastName
+    }
+
+    fun getBirthDate() : LocalDate {
+        return this.birthDate
+    }
+
+    fun getPassword() : String {
+        return this.password
+    }
+
+    fun setFirstName(firstName : String) {
+        this.firstName = firstName
+    }
+
+    fun setLastName(lastName : String) {
+        this.lastName = lastName
+    }
+
+    fun setBirthDate(birthDate : LocalDate) {
+        this.birthDate = birthDate
+    }
+
+    fun setPassword(password : String) {
+        this.password = password
+    }
+
+
 }
