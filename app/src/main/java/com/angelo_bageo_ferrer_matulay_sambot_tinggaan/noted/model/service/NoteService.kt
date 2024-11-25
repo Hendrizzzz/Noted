f4582bef98692fcab78ca392a3e16b0480322d9c
Hendrizzzz <jimhendrixbageo@gmail.com>
2024-11-26 00:56:38 +0800

    Updated Service Classes

--- app/src/main/java/com/angelo_bageo_ferrer_matulay_sambot_tinggaan/noted/model/service/NoteService.kt
+++ app/src/main/java/com/angelo_bageo_ferrer_matulay_sambot_tinggaan/noted/model/service/NoteService.kt
@@ -1,4 +1,15 @@
 package com.angelo_bageo_ferrer_matulay_sambot_tinggaan.noted.model.service
 
 class NoteService {
+    fun addRated() {
+
+    }
+
+    fun addPosted() {
+
+    }
+
+    fun archiveNote() {
+
+    }
 }
--- app/src/main/java/com/angelo_bageo_ferrer_matulay_sambot_tinggaan/noted/model/service/UserService.kt
+++ app/src/main/java/com/angelo_bageo_ferrer_matulay_sambot_tinggaan/noted/model/service/UserService.kt
@@ -1,4 +1,13 @@
 package com.angelo_bageo_ferrer_matulay_sambot_tinggaan.noted.model.service
 
 class UserService {
+
+    fun validateCredentials(email: String, firstName: String, lastName: String, birthDate: String, password: String): Boolean {
+        return true
+    }
+
+    fun validateLogin(email: String, password: String): Boolean {
+        return !(email.isEmpty() || password.isEmpty())
+    }
+
 }
